c99528275d943817ef7a19e27018f8a938e9eddc
bbaransahin <barana340@gmail.com>
2023-02-05 16:12:28 +0300

    created model.py file and initialized the strategy on comments

---
+++ model.py
@@ -0,0 +1,11 @@
+# If the computation costs are practical, will use strategy like AphaGo has.
+# Pre Req:
+# 	1. Stockfish
+
+# Generating games or positions
+
+# Stockfis analyzes
+
+# Creating model
+
+# Train
